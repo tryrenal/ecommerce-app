3531044d41df51e886e6ce571abd19f2ab70b108
redveloper <renalraion@gmail.com>
2025-03-11 10:05:31 +0700

    Initial Commit

---
+++ ecommerce-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ecommerce-app
+//
+//  Created by redveloper on 3/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ecommerce-app/ecommerce_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerce_appApp.swift
+//  ecommerce-app
+//
+//  Created by redveloper on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ecommerce_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
